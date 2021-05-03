fc5f4a9e325af893f67e41bca2eeb2123cbc98a3
Gavin.Xue <xuebailong@hotmail.com>
2021-05-04 07:27:57 +0800

    修改主键类isPrimary属性为Final

--- gtd/src/main/java/org/ageadoc/gtd/hibernate/EntityPrimary.java
+++ gtd/src/main/java/org/ageadoc/gtd/hibernate/EntityPrimary.java
@@ -3,23 +3,18 @@ package org.ageadoc.gtd.hibernate;
 import javax.persistence.GenerationType;
 
 public class EntityPrimary extends EntityField{
-    private Boolean isPrimary = true;
+    private final Boolean isPrimary = Boolean.TRUE;
     private GenerationType strategyType;
     private String sequenceName;
 
     public EntityPrimary(ColumnType columnType, String name, String columnName, GenerationType strategyType, String sequenceName){
         super(columnType, name, columnName, false);
-        this.isPrimary = true;
         this.strategyType = strategyType;
         this.sequenceName = sequenceName;
     }
 
     public Boolean getPrimary() {
         return isPrimary;
-    }
-
-    public void setPrimary(Boolean primary) {
-        isPrimary = primary;
     }
 
     public GenerationType getStrategyType() {
